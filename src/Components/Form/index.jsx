c75666791878dd18b2911dac9c3675c3fee2d5a0
Suprit Beck <suprit.beck@olx.com>
2023-05-04 13:45:46 +0530

    feat: form issue fixed

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -2,10 +2,12 @@ import React, {useContext, useState} from 'react'
 import { useParams } from 'react-router';
 import {useNavigate} from 'react-router-dom'
 import { PortalContext } from '../../App';
+import './form.css'
 const Form = () => {
 const {setPortalData,originalData} = useContext(PortalContext)
 const navigate = useNavigate()
 const {id} = useParams()
+const {name} = originalData.find(item=>item.id ==id) || {}
 const [userName,setUserName] = useState('')
 const [number,setNumber] = useState('')
 const onClickHandler = ()=>{
@@ -24,10 +26,13 @@ const {value} = e.target || {}
 }
   return (
       <>
-    <div>Form {id}</div>
-    <input type='text' required value={userName} onChange={(e)=>setUserName(e.target.value)}/>
-    <input type='text' required value={number} pattern = "[0-9]" onChange={numberChangeHandler}/>
-    <button type='submit' onClick={onClickHandler}>Submit</button>
+    <div className='form'>{name} Form </div>
+    <br/>
+    <input className='form' type='text' placeholder='Enter user name' required value={userName} onChange={(e)=>setUserName(e.target.value)}/>
+    <br/>
+    <input className='form' type='text' placeholder='Enter Phone number' value={number} pattern = "[0-9]" onChange={numberChangeHandler}/>
+    <br/>
+    <button className='form' type='submit' onClick={onClickHandler}>Submit</button>
     </>
   )
 }
